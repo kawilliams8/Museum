481d16e6ff861eb5ac3e38a5c6bedd9440a2a52c
Katie Williams <kawilliams8@gmail.com>
2019-11-04 17:34:56 -0700

    Pass props from Gallery to a ul with dynamic li elements

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="gallery">
     <h1>Gallery</h1>
+      <ul id="artObjectList">
+        <li v-for="artObject in artObjects" v-bind:key="artObject.objectid">
+          {{artObject.title}}
+          {{artObject.dated}}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Gallery',
-  components: {}
+  components: {},
+  props: ['artObjects']
 }
 </script>
 
