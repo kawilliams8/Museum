5db38aa3c11ccacf1239c21b59a20dd545b404cb
Katie Williams <kawilliams8@gmail.com>
2019-11-06 09:04:41 -0700

    Add snapshot test to Header, remove dummy test

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -2,7 +2,9 @@ import { shallowMount } from "@vue/test-utils";
 import Header from "@/components/Header.vue";
 
 describe("Header.vue", () => {
-  it("should pass this dummy test", () => {
-    expect(true).toBe(true);
-  });
+
+  it('should pass the snapshot with all props passed in', () => {
+    const wrapper = shallowMount(Header);
+    expect(wrapper.element).toMatchSnapshot();
+  })
 });
